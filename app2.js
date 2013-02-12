ce2ed55136e150b1148aa419bfb9b54b261ae24f
sdf <aashidham@gmail.com>
2013-02-12 00:28:57 -0500

    got the line-height parameter set using js

--- app2.js
+++ app2.js
@@ -1,3 +1,9 @@
 $(document).on("mobileinit", function(){
   $.mobile.touchOverflowEnabled = true;
 });
+
+//onload
+$(function(){
+	var height = $("#day_header").height();
+	$("#day_header").css("line-height",height+"px");
+});
