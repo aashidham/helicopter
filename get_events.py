77a8aca63dd7a0bb43f43daa685c5fa90f52244b
sdf <aashidham@gmail.com>
2013-02-20 17:43:02 -0500

    added email access to API request and no longer arranging things by day on server

--- get_events.py
+++ get_events.py
@@ -12,7 +12,7 @@ from oauth2client.client import AccessTokenRefreshError
 from oauth2client.client import OAuth2WebServerFlow
 from oauth2client.file import Storage
 
-flow = OAuth2WebServerFlow("771424049558.apps.googleusercontent.com","66_YXKM5FfTAF02nZI3dV9Yn",'https://www.googleapis.com/auth/calendar',redirect_uri='http://helicopter-app.com/callback')
+flow = OAuth2WebServerFlow("771424049558.apps.googleusercontent.com","66_YXKM5FfTAF02nZI3dV9Yn",['https://www.googleapis.com/auth/calendar.readonly','https://www.googleapis.com/auth/userinfo.email'],redirect_uri='http://helicopter-app.com/callback')
 
 def login():
 	return flow.step1_get_authorize_url()
@@ -22,22 +22,24 @@ def callback(code):
 	http = httplib2.Http()
 	http = credentials.authorize(http)
 	service = build('calendar', 'v3', http=http)
-	#now = datetime.datetime.utcnow().isoformat("T")+"Z"
-	#later = datetime.datetime.utcnow()+datetime.timedelta(days=14)
-	#later = later.isoformat("T")+"Z"
+	user_info_service = build('oauth2','v2',http=http)
 	now = datetime.datetime.utcnow()
 	later = now + datetime.timedelta(days=14)
 	before = now - datetime.timedelta(days=1)
+	
+	now = now.isoformat("T")+"Z"
 	later = later.isoformat("T")+"Z"
 	before = before.isoformat("T")+"Z"
-	request = service.events().list(calendarId='primary',timeMax=later,timeMin=before,singleEvents="true")
-	toReturn = defaultdict(list)
+	request = service.events().list(calendarId='primary',timeMax=later,timeMin=now,singleEvents="true")
+	toReturn = []
 	while request != None:
 		response = request.execute()
 		for event in response.get('items', []):
 			epoch_seconds = tf_from_timestamp(event["start"]["dateTime"])
 			currdate = datetime.date.fromtimestamp(epoch_seconds)
 			currdate = currdate.isoformat()
-			toReturn[currdate].append({"summary":event["summary"],"start":event["start"]["dateTime"],"end":event["end"]["dateTime"]})
+			toReturn.append({"summary":event["summary"],"start":event["start"]["dateTime"],"end":event["end"]["dateTime"]})
 		request = service.events().list_next(request, response)
+	email = user_info_service.userinfo().get().execute()["email"]
+	toReturn.append(email)
 	return json.dumps(toReturn,sort_keys=True)
